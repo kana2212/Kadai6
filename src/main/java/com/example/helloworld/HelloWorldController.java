3779711e1164dc093f1446578c09fa2432e65d3e
kana2212 <cikiruki@yahoo.co.jp>
2023-01-27 18:13:00 +0900

    指摘箇所の修正

---
+++ src/main/java/com/example/helloworld/HelloWorldController.java
@@ -0,0 +1,18 @@
+package com.example.helloworld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+
+@RestController
+public class HelloWorldController {
+    @GetMapping("/restgetTime")
+    public String hello() {
+        return """
+                Hello.
+                It works!
+                現在時刻は%sです。
+                """.formatted(LocalDateTime.now());
+    }
+}
